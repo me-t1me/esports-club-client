c2a5ddd08a96e958d4a8a20e6f93b79f566d3fe5
Siddhant Jain <siddhantashoknagar@gmail.com>
2021-08-01 00:15:44 +0530

    closed

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,23 +8,23 @@ import AnimationText from "./AnimationText/AnimationText";
 // import "reactjs-popup/dist/index.css";
 // import { usePopup } from "react-hook-popup";
 
-// import logo from "../../assets/esports_colour.png";
-// const URL = process.env.REACT_APP_API_URL;
-// const MID = process.env.REACT_APP_MID;
+import logo from "../../assets/esports_colour.png";
+const URL = process.env.REACT_APP_API_URL;
+const MID = process.env.REACT_APP_MID;
 
-// const loadRazorpay = (src) => {
-//   return new Promise((resolve) => {
-//     const script = document.createElement("script");
-//     script.src = src;
-//     script.onload = () => {
-//       resolve(true);
-//     };
-//     script.onerror = () => {
-//       resolve(false);
-//     };
-//     document.body.appendChild(script);
-//   });
-// };
+const loadRazorpay = (src) => {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+};
 
 const Form = () => {
   const [isClicked, setClicked] = useState(false);
@@ -44,79 +44,79 @@ const Form = () => {
 
   const alerttoast = (message) => toast.dark(`${message}`);
 
-  // const displayRazorpay = async () => {
-  //   const res = await loadRazorpay(
-  //     "https://checkout.razorpay.com/v1/checkout.js"
-  //   );
+  const displayRazorpay = async () => {
+    const res = await loadRazorpay(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
 
-  //   if (!res) {
-  //     alert("Razorpay sdk failed to load");
-  //     return;
-  //   }
+    if (!res) {
+      alert("Razorpay sdk failed to load");
+      return;
+    }
 
-  //   const data = await fetch(`${URL}/apis/pay`, { method: "POST" }).then((t) =>
-  //     t.json()
-  //   );
+    const data = await fetch(`${URL}/apis/pay`, { method: "POST" }).then((t) =>
+      t.json()
+    );
 
-  //   console.log(data);
+    console.log(data);
 
-  //   const options = {
-  //     key: MID, // Enter the Key ID generated from the Dashboard
-  //     amount: data.amount.toString(), // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-  //     currency: data.currency,
-  //     order_id: data.id,
-  //     name: "Esports Club BPHC",
-  //     description: "Duality Tournament",
-  //     image: { logo },
-  //     handler: function (response) {
-  //       alert(" Payment Successful, Proceed to submit your form. ");
-  //       // window.open("/success", "_blank");
-  //       // alert(response.razorpay_payment_id);
-  //       // alert(response.razorpay_order_id);
-  //       // alert(response.razorpay_signature);
-  //       setStatus("captured");
-  //     },
-  //   };
-  //   const rzp1 = new window.Razorpay(options);
-  //   rzp1.open();
-  //   rzp1.on("payment.failed", function (response) {
-  //     // window.open("/failure", "_blank");
-  //     alert(
-  //       "Payment Failed, You can still submit the form, we will contact you to make payment afterwards. "
-  //     );
-  //     setStatus("not successful");
+    const options = {
+      key: MID, // Enter the Key ID generated from the Dashboard
+      amount: data.amount.toString(), // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: data.currency,
+      order_id: data.id,
+      name: "Esports Club BPHC",
+      description: "Duality Tournament",
+      image: { logo },
+      handler: function (response) {
+        alert(" Payment Successful, Proceed to submit your form. ");
+        // window.open("/success", "_blank");
+        // alert(response.razorpay_payment_id);
+        // alert(response.razorpay_order_id);
+        // alert(response.razorpay_signature);
+        setStatus("captured");
+      },
+    };
+    const rzp1 = new window.Razorpay(options);
+    rzp1.open();
+    rzp1.on("payment.failed", function (response) {
+      // window.open("/failure", "_blank");
+      alert(
+        "Payment Failed, You can still submit the form, we will contact you to make payment afterwards. "
+      );
+      setStatus("not successful");
 
-  //     // alert(response.error.code);
-  //     // alert(response.error.description);
-  //     // alert(response.error.source);
-  //     // alert(response.error.step);
-  //     // alert(response.error.reason);
-  //     // alert(response.error.metadata.order_id);
-  //     // alert(response.error.metadata.payment_id);
-  //   });
-  //   rzp1.on("payment.successful", function (response) {
-  //     // alert(response.error.code);
-  //     // alert(response.error.description);
-  //     // alert(response.error.source);
-  //     // alert(response.error.step);
-  //     // alert(response.error.reason);
-  //     // alert(response.error.metadata.order_id);
-  //     // alert(response.error.metadata.payment_id);
-  //   });
-  // };
+      // alert(response.error.code);
+      // alert(response.error.description);
+      // alert(response.error.source);
+      // alert(response.error.step);
+      // alert(response.error.reason);
+      // alert(response.error.metadata.order_id);
+      // alert(response.error.metadata.payment_id);
+    });
+    rzp1.on("payment.successful", function (response) {
+      // alert(response.error.code);
+      // alert(response.error.description);
+      // alert(response.error.source);
+      // alert(response.error.step);
+      // alert(response.error.reason);
+      // alert(response.error.metadata.order_id);
+      // alert(response.error.metadata.payment_id);
+    });
+  };
 
-  // const pay = () => {
-  //   displayRazorpay();
-  //   // try {
+  const pay = () => {
+    displayRazorpay();
+    // try {
 
-  //   //   // window.open(`${URL}/apis/pay`, "_blank"); // https://esport-club-server.herokuapp.com , http://localhost:3005
-  //   //   // alert(
-  //   //   //   "Please Submit after paying fee, otherwise Registration will be invalid"
-  //   //   // );
-  //   // } catch (error) {
-  //   //   console.log(error.response.data);
-  //   // }
-  // };
+    //   // window.open(`${URL}/apis/pay`, "_blank"); // https://esport-club-server.herokuapp.com , http://localhost:3005
+    //   // alert(
+    //   //   "Please Submit after paying fee, otherwise Registration will be invalid"
+    //   // );
+    // } catch (error) {
+    //   console.log(error.response.data);
+    // }
+  };
 
   // const getStatus = async () => {
   //   const response = await Api.get("/2");
@@ -170,6 +170,7 @@ const Form = () => {
 
   return (
     <div>
+      <div className="closed">Registration Closed</div>
       <div className="toast">
         <ToastContainer />
       </div>
@@ -278,23 +279,12 @@ const Form = () => {
           </div>
 
           <div className="form-pay">
-            <h4>
+            {/* <h4>
               UPI ID: <span className="tm">siddhantashoknagar@oksbi</span>
               <br />
               note - put your name in comments
-            </h4>
-            {/* <Popup
-              trigger={
-                <button className="pay">
-                  <span className="pay-first">Pay&nbsp;</span>
-                  <span className="tm">Now</span>
-                </button>
-              }
-              modal
-            >
-              <div className="popup"> Modal content </div>
-            </Popup> */}
-            {/* <button
+            </h4> */}
+            <button
               className="pay"
               required
               onClick={() => {
@@ -317,7 +307,7 @@ const Form = () => {
             >
               <span className="pay-first">Pay&nbsp;</span>
               <span className="tm">Now</span>
-            </button> */}
+            </button>
           </div>
 
           <div className="form-submit">
